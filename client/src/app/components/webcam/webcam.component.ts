7a7ebe56b11198d3797e05e4d56f102bba466eb0
Peter Schramm <peter_schramm@live.de>
2023-09-12 16:01:12 +0200

    Added face detection

--- client/src/app/components/webcam/webcam.component.ts
+++ client/src/app/components/webcam/webcam.component.ts
@@ -1,5 +1,8 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { EventDispatcherService } from 'src/app/services/event-dispatcher.service';
+import * as faceapi from 'face-api.js';
+import { HttpClient } from '@angular/common/http';
+import { map } from "rxjs";
 
 @Component({
   selector: 'beer-webcam',
@@ -12,6 +15,7 @@ export class WebcamComponent implements AfterViewInit {
 
   constructor(
     private eventDispatcher: EventDispatcherService,
+    private httpClient: HttpClient,
   ) { }
 
   async ngAfterViewInit(): Promise<void> {
@@ -24,7 +28,41 @@ export class WebcamComponent implements AfterViewInit {
       const videoElement = this.videoElementRef.nativeElement as HTMLVideoElement;
       videoElement.srcObject = stream;
       await videoElement.play();
+
+      if (this.videoElementRef) {
+        const videoElement = this.videoElementRef.nativeElement as HTMLVideoElement;
+        const canvas = await faceapi.createCanvasFromMedia(videoElement);
+        canvas.style.position = 'absolute';
+        videoElement.parentElement?.appendChild(canvas);
+
+        const { width, height } = videoElement.getBoundingClientRect()
+        faceapi.matchDimensions(canvas, { width, height });
+
+        const modelsPath = '../assets/models/';
+        await Promise.all([
+          faceapi.nets.faceExpressionNet.loadFromUri(modelsPath),
+          faceapi.nets.faceLandmark68Net.loadFromUri(modelsPath),
+          faceapi.nets.tinyFaceDetector.loadFromUri(modelsPath),
+        ]);
+
+        setInterval(this.detectAndDraw(videoElement, { width, height }, canvas), 100);
+      }
     }
   }
 
+
+  private detectAndDraw(videoElement: HTMLVideoElement, displaySize: { width: number; height: number; }, canvas: HTMLCanvasElement): () => void {
+    return async () => {
+      const faceDetections = faceapi.detectAllFaces(videoElement, new faceapi.TinyFaceDetectorOptions({}));
+      const detections = await faceDetections
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      // console.log(detections);
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvas.getContext("2d")?.clearRect(0, 0, displaySize.width, displaySize.height);
+      // faceapi.draw.drawDetections(canvas, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    };
+  }
 }
